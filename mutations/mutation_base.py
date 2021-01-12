3ea17cd025c929dd8727b6eb65194be21540c35f
Diego Romero <dfrn2003@gmail.com>
2021-01-12 09:55:05 -0600

    Moving the format functions

--- mutations/mutation_base.py
+++ mutations/mutation_base.py
@@ -1,7 +1,6 @@
-from mutations.mutation_argument import MutationArgument
-from typing import Generator
 import graphene
 from .validator import Validator
+from .utils.formatting import format_extra_arguments, format_graphene_arguments
 
 
 class MutationBase(graphene.Mutation):
@@ -14,84 +13,13 @@ class MutationBase(graphene.Mutation):
     def mutate(cls, root, info, **kwargs):
         pass
 
-    def format_extra_arguments(extra_arguments: list,  is_required: list = []) -> Generator:
-        if Validator.validate_extra_arguments(extra_arguments):
-            for argument in extra_arguments:
-
-                argument_names = argument[0]
-                scalar = argument[1]
-
-                display_name = ""
-                property_name = ""
-                # If argument_name has property_name
-                if isinstance(argument_names, tuple):
-                    display_name = argument_names[0]
-                    property_name = argument_names[1]
-                # Else property_name will be the same as display_name
-                else:
-                    display_name = argument_names
-                    property_name = argument_names
-
-                is_property = True
-                if len(argument) > 2:
-                    is_property = argument[2]
-
-                # If name is required
-                arg_is_required = display_name in is_required
-
-                yield MutationArgument(
-                    display_name=display_name,
-                    property_name=property_name,
-                    is_property=is_property,
-                    is_required=arg_is_required,
-                    graphene_scalar=scalar
-                )
-
-    def format_graphene_arguments(graphene_type, is_required=[]):
-        # Accessing specific place in graph type that has all options needed
-        graphene_type_options = graphene_type._meta.class_type._meta.__dict__
-        # Getting all the fields in the type
-        fields = graphene_type_options.get("fields")
-
-        for name, field in fields.items():
-            # Field comes from the type directly, so it's assumed that is a property
-            is_property = True
-            arg_is_required = name in is_required
-
-            is_relationship = False
-            scalar = None
-            if field_type := field.__dict__.get("_type"):
-                # If it is not a graphene scalar, but a graphene structure like,
-                # nonNull, the actual scalar will be inside of the "of_type"
-                # key in the structure object.__dict__
-                # If field it's not an structure, then
-                # the field is the scalar
-                gp_of_type = field_type.__dict__.get("_of_type")
-
-                scalar = gp_of_type(required=arg_is_required) if gp_of_type else field_type(
-                    required=arg_is_required)
-
-            # If the field is a relationship
-            elif callable(field.type):
-                is_relationship = True
-                scalar = graphene.String(required=True)
-
-            yield MutationArgument(
-                display_name=name,
-                property_name=name,
-                graphene_scalar=scalar,
-                is_required=arg_is_required,
-                is_property=is_property,
-                is_relationship=is_relationship,
-            )
-
     def set_arguments(self, options):
         # Getting and Setting Extra Arguments
-        extra_arguments = self.format_extra_arguments(
-            options.get("extra_arguments"))
+        extra_arguments = format_extra_arguments( 
+            extra_arguments=options.get("extra_arguments"))
 
-        self.set_graphene_type(self, options)
-        graphene_type_argument = self.format_graphene_arguments(
+        self.set_graphene_type(options)
+        graphene_type_argument = format_graphene_arguments(
             self.graphene_type)
 
         if not hasattr(self, "Arguments"):
