687093b7539258b420e2663173f822c10d313e39
Diego Romero <dfrn2003@gmail.com>
2021-01-15 11:56:33 -0600

    change

--- mutations/test/test_mutation_base.py
+++ mutations/test/test_mutation_base.py
@@ -20,38 +20,31 @@ class MutationBaseSetFunctionsTestCase(TestCase):
         self.assertTrue(issubclass(
             self.mutation_base.graphene_type, IsolatedModelType))
 
-    def test_set_arguments(self):
+    def test_set_graphene_arguments(self):
         # Setting the arguments
         options = {
-            "extra_arguments" : [("ext_arg", graphene.String())],
             "graphene_type" : IsolatedModelType
         }
-        self.mutation_base.set_arguments(options)
+        self.mutation_base.set_graphene_arguments(options)
 
         # Testing for arguments in graphene_type
         arg_name_in_type = IsolatedModelType().__dict__
         for arg in arg_name_in_type:
             self.assertTrue(hasattr(self.mutation_base.Arguments, arg))
 
+        
+    def test_set_extra_arguments(self):
+        options = {
+            "extra_arguments" : [("ext_arg", graphene.String())],
+        }
+        self.mutation_base.set_extra_arguments(options)
+
         # Testing for extra_arguments
         ext_args = options.get("extra_arguments")
         for arg in ext_args:
             self.assertTrue(hasattr(self.mutation_base.Arguments, arg[0]))
-        
-        # Checking for leakage
-        empty_options = {
-            "graphene_type" : RelationshipReceiverModelType,
-            "extra_arguments" : []
-        }
-        self.mutation_base_second.set_arguments(empty_options)
-
-        for arg in arg_name_in_type:
-            if not arg == "name" and not arg =="id":
-                self.assertFalse(hasattr(self.mutation_base_second.Arguments, arg))
-
-        for arg in ext_args:
-            self.assertFalse(hasattr(self.mutation_base_second.Arguments, arg[0]))
     
+
     def test_get_model(self):
         options = {
             "graphene_type": IsolatedModelType
