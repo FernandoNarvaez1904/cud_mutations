f8b7aca8da92bcc910c4259a3156de3be23d4b1c
Diego Romero <dfrn2003@gmail.com>
2021-01-18 10:22:05 -0600

    add init_with_meta

--- mutations/delete_mutation.py
+++ mutations/delete_mutation.py
@@ -1,13 +1,37 @@
 import graphene
 from .mutation_base import MutationBase
-from .utils import enforce_custom_auth_decorator, get_graphene_arguments
+from .utils.security import enforce_custom_auth_decorator
 
 
 class DeleteMutation(MutationBase):
 
-    class Arguments:
-        id = graphene.String(required=True)
+    @classmethod
+    def __init_subclass_with_meta__(
+        cls,
+        interfaces=(),
+        possible_types=(),
+        default_resolver=None,
+        _meta=None,
+        **options
+    ):
+        cls.set_graphene_type(cls, options)
 
+        cls.set_extra_arguments(options)
+
+        cls.set_custom_auth(cls, options)
+
+        cls.set_extra_info(cls, options)
+
+        return super().__init_subclass_with_meta__(
+            interfaces=interfaces,
+            possible_types=possible_types,
+            default_resolver=default_resolver,
+            _meta=_meta,
+            **options
+        )
+    class Arguments:
+        id = graphene.ID(required=True)
+    
     @classmethod
     @enforce_custom_auth_decorator
     def mutate(cls, root, info, **kwargs):
@@ -21,6 +45,6 @@ class DeleteMutation(MutationBase):
         except Exception:
             # If quering failed returning error
             errors = [f"{model.__name__} with id '{id}' doesn't exist."]
-            return DeleteMutation(errors=errors, completed=False, )
+            return DeleteMutation(messages=errors, completed=False, )
         # Returning successfully
         return DeleteMutation(completed=True)
