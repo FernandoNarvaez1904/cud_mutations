eda06de3161197d127fd7b22da91449d9d3714f8
Diego Romero <dfrn2003@gmail.com>
2021-01-19 14:19:51 -0600

    Passaing context instead of user

--- mutations/utils/security.py
+++ mutations/utils/security.py
@@ -7,12 +7,12 @@ def enforce_custom_auth_decorator(function):
         info = args[2]
 
         # Validating auth
-        auth = cls.custom_auth(cls=cls, user=info.context.user, **kwargs)
+        auth = cls.custom_auth(cls=cls, context=info.context, **kwargs)
 
         # If auth failed, returning mutation with errors
         if not auth[0]:
             return cls(completed=False, errors=auth[1])
-
+        print("done")
         # If auth succeeded continuing with the execution
         rt = function(*args, **kwargs)
         return rt
