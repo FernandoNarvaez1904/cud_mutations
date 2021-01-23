3a9ad85b43929e61d7110af8bc9df443d8aff5d1
Diego Romero <dfrn2003@gmail.com>
2021-01-22 18:26:54 -0600

    Created bases for create mutation, missing  mutation method

---
+++ test_mutations/mutations/create_mutations.py
@@ -0,0 +1,28 @@
+from mutations.create_mutation import CreateMutation
+from graphene import ObjectType
+from ..graphene_django_type import (
+    IsolatedModelType,
+    RelationshipReceiverModelType,
+    RelationshipSenderModelType,
+)
+
+
+class CreateIsolatedType(CreateMutation):
+    class Meta:
+        graphene_type = IsolatedModelType
+
+
+class CreateRelationshipReceiverType(CreateMutation):
+    class Meta:
+        graphene_type = RelationshipReceiverModelType
+
+
+class CreateRelationshipSenderType(CreateMutation):
+    class Meta:
+        graphene_type = RelationshipSenderModelType
+
+
+class CreateMutations(ObjectType):
+    create_isolated_type = CreateIsolatedType.Field()
+    create_relationship_receiver_type = CreateRelationshipReceiverType.Field()
+    create_relationship_sender_type = CreateRelationshipSenderType.Field()
